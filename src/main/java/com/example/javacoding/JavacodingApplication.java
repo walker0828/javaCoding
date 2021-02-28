89504897e26cc485123777a0a12cb312274ea62a
yachao.ni <yachao.ni@ipinyou.com>
2021-02-28 15:42:25 +0800

    第一版本

---
+++ src/main/java/com/example/javacoding/JavacodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacodingApplication.class, args);
+    }
+
+}
