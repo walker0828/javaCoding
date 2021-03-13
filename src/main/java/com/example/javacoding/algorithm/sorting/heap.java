0e64ce7410f58c08e7cbd0bf90b52fd7ec42d64f
yachao.ni <yachao.ni@ipinyou.com>
2021-03-13 13:19:48 +0800

    提交代码

--- src/main/java/com/example/javacoding/algorithm/sorting/heap.java
+++ src/main/java/com/example/javacoding/algorithm/sorting/heap.java
@@ -4,9 +4,47 @@ import java.util.Arrays;
 
 // 平均时间复杂度 O(nlogn)
 public class heap {
-    public void heap(int[] sourceArray) {
+    public static void heap(int[] sourceArray) {
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
         // 构建 最大堆
+        int len = arr.length;
+        buildMaxHeap(arr,len);
+        System.out.println("end");
+    }
 
+    private static void buildMaxHeap(int[] arr, int len) {
+        for (int i = (int) Math.floor(len / 2); i >= 0; i--) {
+            heapify(arr, i, len);
+        }
+    }
+
+    private static void heapify(int[] arr, int i, int len) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+
+        if (left < len && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < len && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, largest, len);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{9,8,7,6,5,4,3,2,1};
+        heap(array);
     }
 }
--- src/main/java/com/example/javacoding/algorithm/sorting/quick.java
+++ src/main/java/com/example/javacoding/algorithm/sorting/quick.java
@@ -1,8 +1,47 @@
 package com.example.javacoding.algorithm.sorting;
 
+import java.util.Arrays;
+
 public class quick {
     //快速排序
-    public void quick(int[] originData) {
+    public static void quick(int[] sourceArray) {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("end");
+    }
 
+    private static int[] quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(arr, left, right);
+            quickSort(arr, left, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, right);
+        }
+        return arr;
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+        // 设定基准值（pivot）
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{9,8,7,6,5,4,3,2,1};
+        quick(array);
     }
 }
