d62a01ba4a133454e53810e9e2854e68f5d14b75
yachao.ni <yachao.ni@ipinyou.com>
2021-03-31 19:31:20 +0800

    提交代码

--- src/main/java/com/example/javacoding/dataStructure/tree/BBST.java
+++ src/main/java/com/example/javacoding/dataStructure/tree/BBST.java
@@ -1,5 +1,21 @@
 package com.example.javacoding.dataStructure.tree;
 
+import com.example.javacoding.dataStructure.Node;
+
+import java.util.Comparator;
+
 public class BBST<E> extends BinarySearchTree<E> {
+    public BBST() {
+
+    }
+
+    public BBST(Comparator<E> comparator) {
+        this.comparator = comparator;
+    }
+
+    protected void rotateLeft(Node<E> node) {
+//        Node<E> child =node.right;
+    }
+
 
 }
