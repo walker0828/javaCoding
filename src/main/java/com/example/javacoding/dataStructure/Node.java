7637876c7010ddefe2bc75560466dc0c22915c60
yachao.ni <yachao.ni@ipinyou.com>
2021-04-09 08:37:39 +0800

    、、、新

--- src/main/java/com/example/javacoding/dataStructure/Node.java
+++ src/main/java/com/example/javacoding/dataStructure/Node.java
@@ -1,11 +1,3 @@
 package com.example.javacoding.dataStructure;
 
-public class Node<E> {
-    E element;
 
-    Node<E> parent;
-
-    public Node(E element,Node<E> parent) {
-
-    }
-}
---
+++ src/main/java/com/example/javacoding/systemDesign/MyHashMap.java
@@ -0,0 +1,5 @@
+package com.example.javacoding.systemDesign;
+
+public class MyHashMap {
+
+}
